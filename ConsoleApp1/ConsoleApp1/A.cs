ab8db98bab5acdb8ef22bd43e574144accbdeeda
Nurigreen <nurigreen@yandex.ru>
2021-12-18 00:04:27 +0300

    Second Lab

---
+++ ConsoleApp1/ConsoleApp1/A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class A
+    {    
+        public float[] linearEquation(float a, float b)
+        {
+            if (a == 0)
+            {
+                return null;
+            }
+            float[] x = new float[1];
+            return x = new float[] { -b / a };
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/B.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class B : A
+    {
+        protected float discriminant(float a, float b, float c)
+        {
+            return b * b - 4 * a * c;
+        }
+
+        protected float[] quadraticEquation(float a, float b)
+        {
+            if (a == 0)
+            {
+                return linearEquation(1, 1);
+            }
+
+            float d = discriminant(1, 1, 1);
+            if (d < 0)
+            {
+                return null;
+            }
+
+            if (d == 0)
+            {
+                return new float[] { -b / (2 * a) };
+            }
+
+            d = (float)Math.Sqrt(d);
+
+            float[] array = new float[2];
+            array[1] = (float)(-b - d / (2 * a));
+            array[2] = (float)((-b + d / (2 * a)));
+            return array;
+        }
+    }
+}
