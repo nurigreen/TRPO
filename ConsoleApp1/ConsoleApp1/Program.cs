e1cd9193bf40a1e19f26021753ccb3c4ce02cd1e
Nurigreen <nurigreen@yandex.ru>
2021-12-17 23:52:22 +0300

    First Lab

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class A
+        {
+
+        }
+
+        public class B : A
+        {
+            public object a;
+        }
+
+        public class C : B
+        {
+            public object b;
+            public object c;
+        }
+
+        public static void Main(String[] ars)
+        {
+            A a1 = new A();
+            A a2 = new A();
+            A a3 = new A();
+            B b4 = new B();
+            b4.a = a1;
+
+            C c5 = new C();
+            c5.a = a2;
+            c5.b = b4;
+            c5.c = a3;
+        }
+    }
+}
